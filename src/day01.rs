e4f95fd5c50abf7a419fa7672a45dc739219b508
Steve Astels <sastels@gmail.com>
2020-12-27 19:34:43 -0500

    day1

--- src/day01.rs
+++ src/day01.rs
@@ -1,7 +1,18 @@
-pub fn solve_a(_data: &[String]) -> i32 {
-    unimplemented!()
+pub fn solve_a(data: &[String]) -> i32 {
+    data[0].chars().map(|c| if c == '(' { 1 } else { -1 }).sum()
 }
 
-pub fn solve_b(_data: &[String]) -> i32 {
-    unimplemented!()
+pub fn solve_b(data: &[String]) -> usize {
+    1 + data[0]
+        .chars()
+        .map(|c| if c == '(' { 1 } else { -1 })
+        .scan(0, |state, x| {
+            *state = *state + x;
+            Some(*state)
+        })
+        .enumerate()
+        .filter(|(_, x)| *x == -1)
+        .next()
+        .unwrap()
+        .0
 }
