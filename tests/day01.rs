164d865fa577f9e1bf51445d5093c5d050b017fb
Steve Astels <sastels@gmail.com>
2020-12-27 19:37:10 -0500

    tests

--- tests/day01.rs
+++ tests/day01.rs
@@ -2,15 +2,13 @@ use advent_2015::day01::{solve_a, solve_b};
 use advent_2015::utils::read_lines;
 
 #[test]
-#[ignore]
 fn a() {
-    let data = read_lines("./data/day01_test.txt");
-    assert_eq!(solve_a(&data), 0);
+    let data = read_lines("./data/day01.txt");
+    assert_eq!(solve_a(&data), 232);
 }
 
 #[test]
-#[ignore]
 fn b() {
-    let data = read_lines("./data/day01_test.txt");
-    assert_eq!(solve_b(&data), 0);
+    let data = read_lines("./data/day01.txt");
+    assert_eq!(solve_b(&data), 1783);
 }
